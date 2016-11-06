85bfb0ead046417e934f29a7ba8697d3f1938b21
Crazy Bob of Crazy Bob's Mattress Warehouse <Aaron@Aarons-MBP.attlocal.net>
2016-11-06 16:37:03 -0500

    i initiallizes projcet

---
+++ New Unity Project/Assets/PlayerController.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerController : MonoBehaviour {
+	public int moveSpeed;
+	public int jumpHeight;
+
+	Rigidbody2D rb2D;
+
+	void Start () {
+		rb2D = GetComponent<Rigidbody2D>();
+	}
+	
+	void Update () {
+		Vector2 moveDir = new Vector2 (Input.GetAxisRaw ("Horizontal") * moveSpeed, rb2D.velocity.y);
+		rb2D.velocity = moveDir;
+		if (Input.GetKeyDown (KeyCode.W)) {
+			rb2D.AddForce (new Vector2 (0, jumpHeight));
+		}
+	}
+}
